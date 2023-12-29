5fe41aca302c05266b6e454c4312755c06680e9c
jenna-stover.github.io <jenna.stover810@gmail.com>
2023-12-29 17:48:53 -0500

    updated website

---
+++ apptPage/appointment.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const showEmailResult = async (e) => { 
+        e.preventDefault();
+        const result = document.getElementById("result");
+        let response = await getEmailResult();
+        if(response.status == 200) {
+            result.innerHTML = "Email request sent!";
+        } else {
+            result.innerHTML = "Error; email did not send";
+        }
+    };
+
+    const getEmailResult = async (e) => {
+    const form = document.getElementById("appointment-form");
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+    const result = document.getElementById("result");
+    result.innerHTML = "Sending..."
+
+    try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: json,
+        });
+        return response;
+    }
+    catch(error) {
+        console.log(error);
+        document.getElementById("result").innerHTML = "Error; email did not send";
+    }
+};
+
+const contactForm = document.getElementById("appointment-form");
+    if (contactForm) {
+        contactForm.onsubmit = showEmailResult;
+    }
+
+});
